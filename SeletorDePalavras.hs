f9cc04ab86c35bde82a324a338caabf0c671ac00
João Vitor <joao.luciano@ccc.ufcg.edu.br>
2022-03-04 00:30:43 -0300

    modularizando

--- SeletorDePalavras.hs
+++ SeletorDePalavras.hs
@@ -1,14 +1,15 @@
-import System.IO
+module SeletorDePalavras where
+    import System.IO
 
-main :: IO()
-main = do
-    json <- openFile "palavras.json" ReadMode
-    numFile <- openFile "num.txt" ReadWriteMode
-    arr <- hGetLine json
-    num <- hGetLine numFile
-    hClose json
-    let a = read arr :: [String]
-    let n = read num :: Int
-    hClose numFile
-    writeFile "num.txt" (show ((n + 1) `mod` length a))
-    print (a !! n)
+    selectorWord :: IO String
+    selectorWord = do
+        json <- openFile "palavras.json" ReadMode
+        numFile <- openFile "num.txt" ReadWriteMode
+        arr <- hGetLine json
+        num <- hGetLine numFile
+        hClose json
+        let a = read arr :: [String]
+        let n = read num :: Int
+        hClose numFile
+        writeFile "num.txt" (show ((n + 1) `mod` length a))
+        return (a !! n)
