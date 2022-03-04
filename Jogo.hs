48f22668071cf9980bb0fcada9c8bb94fdd035d5
Raisson Souto <raisson.souto@ccc.ufcg.edu.br>
2022-03-03 18:15:35 -0800

    Fixing Jogo

--- Jogo.hs
+++ Jogo.hs
@@ -3,6 +3,7 @@ module Jogo where
     import System.IO
     import ColorfulPrint
     import GuessChecker
+    import Data.Char (isLetter, toLower)
 
     toLowerString :: [Char] -> [Char]
     toLowerString str = [ toLower x | x <- str]
@@ -11,22 +12,26 @@ module Jogo where
     checkInputLength texto = do
         length texto /= 5
     
-    jogo::[[String]]->Int->IO()
-    jogo (tentativas:correcao) recursao = do
+    append :: String -> [String] -> [String]
+    append a [] = [a]
+    append a (x:xs) = x : append a xs
+    
+    jogo::[String]->[String]->Int->IO()
+    jogo tentativas metadados recursao = do
     
         newGuess <- getLine
         if checkInputLength newGuess
         then do
             putStr "Palavra com tamanho errado. Tente novamente: "
             hFlush stdout
-            jogo (tentativas:metadados) recursao
+            jogo tentativas metadados recursao
         else do
-            let newTentativas = (tentativas ++ newGuess)
-            let metadado = guessChecker  selectWord
-            let newMetadados = (metadados ++ metadado)
+            let newTentativas = append newGuess tentativas
+            let metadado = guessChecker  newGuess "teste" --selectWord
+            let newMetadados = append metadado metadados
 
-            putStrLn (colorfulPrint tentativas metadados)
+            putStrLn (colorfulPrint newTentativas newMetadados)
             
             if metadado == "VVVVV" then putStrLn "Voce ganhou!"
             else if recursao > 3 then putStrLn "Você perdeu!"
-            else jogo (tentativas:metadados) (recursao+1)
+            else jogo tentativas metadados (recursao+1)
