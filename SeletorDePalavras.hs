3e4ac4cfe81ebfeb499a9aa63415e8acf07cbed2
raissonsouto <raisson.souto@ccc.ufcg.edu.br>
2022-03-06 15:34:29 -0300

    concertando erro do SeletorDePalavras e GuessChecker

--- SeletorDePalavras.hs
+++ SeletorDePalavras.hs
@@ -1,20 +1,12 @@
 module SeletorDePalavras where
     import System.IO
     import System.Random
-    
-    randomNopik::Int->Int
-    randomNopik length = do
-        randomRIO(0,length-1):: IO Int
         
-    selectorWord :: IO String
+    selectorWord::IO String
     selectorWord = do
-        let words = getWords
-        let selectedIndex = randomNopik (length words)
-        return (words !! selectedIndex)
-
-    getWords:: [String]
-    getWords = do
         json <- openFile "palavras.json" ReadMode
         arr <- hGetLine json
         hClose json
-        read arr :: [String]
+        let a = read arr :: [String]    
+        n <- randomRIO(0,(length a)-1)
+        return (a !! n)
